af390b45649f4141197048aa55b8c11e6e4d1545
ChandraGunawanWijaya <161033234+ChandraGunawanWijaya@users.noreply.github.com>
2025-02-20 20:27:05 +0700

    chancahn

---
+++ daftar_karyawan/lib/main.dart
@@ -0,0 +1,81 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:daftar_karyawan/models/karyawan.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  Future<List<Karyawan>> _readJsonData() async {
+    final String response = await rootBundle.loadString('assets/karyawan.json');
+    final List<dynamic> data = json.decode(response);
+    return data.map((json) => Karyawan.fromJson(json)).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Daftar Karyawan"),
+      ),
+      body: FutureBuilder<List<Karyawan>>(
+        future: _readJsonData(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return Center(child: Text("Error loading data: ${snapshot.error}"));
+          }
+
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(
+                    snapshot.data![index].nama,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Umur: ${snapshot.data![index].umur}'),
+                      Text(
+                        'Alamat: ${snapshot.data![index].alamat.jalan}, ${snapshot.data![index].alamat.kota}',
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          }
+
+          return const Center(child: Text("No data available"));
+        },
+      ),
+    );
+  }
+}
